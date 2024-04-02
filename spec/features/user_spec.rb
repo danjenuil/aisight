f914980e2f2c9a24c30ef12f7d684a289d835a02
Danny Jenuil <45913874+danjenuil@users.noreply.github.com>
2024-04-03 01:31:34 +0800

    Add feature spec to test for presence of Sign In page on landing page.

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'User' do
+  context 'homepage' do
+    before do
+      visit '/'
+    end
+
+    it 'display link to sign into AiSight' do
+      expect(page).to have_text('Sign in to AiSight')
+      expect(page).to have_field('#user_email')
+      expect(page).to have_field('#user_password')
+      expect(page).to have_button('Sign In')
+    end
+  end
+end
