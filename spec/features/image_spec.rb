3554e2c8e100166114cba79ad2b5b21596c213d3
Danny Jenuil <45913874+danjenuil@users.noreply.github.com>
2024-04-07 17:26:10 +0800

    Add test for viewing image/show page.

--- spec/features/image_spec.rb
+++ spec/features/image_spec.rb
@@ -3,6 +3,7 @@ require 'faker'
 
 RSpec.describe 'Image' do
   before do
+    Faker::Config.random = Random.new(10)
     # create user and corresponding images (25 of them), using FactoryBot
     @user = FactoryBot.create(:user)
 
@@ -12,11 +13,25 @@ RSpec.describe 'Image' do
   end
 
   it 'allows user to see a list of uploaded images' do
-    sign_in @user
-    visit "/images"
+
     expect(page).to have_text('Images')
     within('table') do
       expect(page).to have_selector('tr', count: 26) # 25 images + 1 header row
     end
+  end
+
+  it 'allows user to see individual image on its own page' do
+    visit "/images"
+    image_title = nil
+
+    # Assuming the 2nd cell in the first row triggers the action
+    within('table tbody tr:first-child') do
+      find('td:last-child').click_link('View')
+    end
+
+    expect(current_path).to match(/images\/\d+/)
+    expect(page).to have_text('Images / Lake Tyson')
+    expect(page).to have_link('Edit')
+    expect(page).to have_link('Delete')
   end
 end
