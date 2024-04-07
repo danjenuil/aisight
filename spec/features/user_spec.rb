badd604bd84b7b7452cf6d48008bbd286fe366dd
Danny Jenuil <45913874+danjenuil@users.noreply.github.com>
2024-04-07 16:30:48 +0800

    Add test for ensuring that when a user is logged in, going to homepage will redirect to the list of images uploaded by the user.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,15 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'User' do
   context 'homepage' do
-    before do
-      visit '/'
+    context 'when user is not signed in' do
+      before do
+        visit '/'
+      end
+
+      it 'display link to sign into AiSight' do
+        expect(page).to have_text('Sign in to AiSight')
+        expect(page).to have_field('user[email]')
+        expect(page).to have_field('user[password]')
+        expect(page).to have_button('Sign In')
+      end
     end
 
-    it 'display link to sign into AiSight' do
-      expect(page).to have_text('Sign in to AiSight')
-      expect(page).to have_field('user[email]')
-      expect(page).to have_field('user[password]')
-      expect(page).to have_button('Sign In')
+    context 'when user is signed in' do
+      before do
+        @user = FactoryBot.create(:user)
+        sign_in @user
+        visit '/'
+      end
+
+      it 'redirects to images page' do
+        expect(current_path).to eq('/images')
+      end
     end
   end
 end
