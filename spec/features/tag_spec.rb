817a3886c4db927e42dae3ca64810b0f58a0bc86
Danny Jenuil <45913874+danjenuil@users.noreply.github.com>
2024-04-10 13:12:49 +0800

    Add test for tag/show page.

--- spec/features/tag_spec.rb
+++ spec/features/tag_spec.rb
@@ -3,15 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Tag' do
   let(:user) { FactoryBot.create(:user) }
 
+  before(:each) do
+    sign_in user
+  end
+
   context 'index page' do
     it 'list all of the tags that the user have' do
       tag_names = ['House', 'Glass', 'Sky']
-
       tag_names.each_with_index do |tag_name, index|
         FactoryBot.create(:tag, name: tag_name, images: FactoryBot.create_list(:image, index + 1, user: user))
       end
 
-      sign_in user
+
       visit '/tags'
       expect(page).to have_text('Tags')
       within('table') do
@@ -25,6 +28,17 @@ RSpec.describe 'Tag' do
           expect(find('tr:nth-child(2) td:nth-child(2)')).to have_text '2'
           expect(find('tr:nth-child(3) td:nth-child(2)')).to have_text '3'
         end
+      end
+    end
+  end
+
+  context 'show page' do
+    it 'show all images that have the same tag' do
+      tag = FactoryBot.create(:tag, name: 'House', images: FactoryBot.create_list(:image, 3, user: user))
+      visit "/tags/#{tag.id}"
+      expect(page).to have_text('Tags / House')
+      within('table') do
+        expect(page).to have_selector('tr', count: 4) # 3 images + 1 header row
       end
     end
   end
